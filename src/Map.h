00bca90bffd87f9b30ee6da0bada6c88f5511d85
oleg <o.gasta@list.ru>
2024-10-09 22:25:45 +0300

    New inctances

---
+++ src/Map.h
@@ -0,0 +1,9 @@
+#include "PointLib.h"
+
+class Map{
+    public:
+      int initMap();
+      Point getPoint(int x, int y, int z);
+      void addPoint(Point point);
+
+}
---
+++ src/PointLib.h
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+struct Point {
+    int x = 0;
+    int y = 0;
+    int z = 0;
+}
